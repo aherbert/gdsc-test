fe8b0392cb47fc88d26c27ab21ec2b8c23cb81b1
aherbert <a.herbert@sussex.ac.uk>
2018-11-30 15:41:10 +0000

    Updated to auto publish snapshots

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    #mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true
-    #exit $?
+# Simple check for a SNAPSHOT version
+grep '<version>.*SNAPSHOT</version>' $DEPLOY_DIR/../pom.xml -q
+
+# Run if a SNAPSHOT and not a pull request or tagged
+if [[ $? == 0 && $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_TAG == "" ]]; then
+    mvn deploy -P snapshot-release --settings $DEPLOY_DIR/settings.xml -DskipTests=true
+    # The snapshot-release profile will fail if the version is not a SNAPSHOT.
+    # It will also fail if the release failed.
+    exit $?
 fi
