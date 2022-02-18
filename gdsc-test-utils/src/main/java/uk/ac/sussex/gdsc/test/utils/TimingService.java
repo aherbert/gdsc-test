18c2d44784fc68936ad52d7367164be685d56c81
Alex Herbert <a.herbert@sussex.ac.uk>
2022-02-18 09:51:02 +0000

    Use System.lineSeparator()

--- gdsc-test-utils/src/main/java/uk/ac/sussex/gdsc/test/utils/TimingService.java
+++ gdsc-test-utils/src/main/java/uk/ac/sussex/gdsc/test/utils/TimingService.java
@@ -38,11 +38,7 @@ public class TimingService {
   /**
    * The new line used in the report. Equal to {@code System.getProperty("line.separator")}.
    */
-  public static final String NEW_LINE;
-
-  static {
-    NEW_LINE = System.getProperty("line.separator");
-  }
+  public static final String NEW_LINE = System.lineSeparator();
 
   /** The number of timing runs. */
   private int runs;
