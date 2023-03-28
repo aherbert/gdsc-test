4d4fb15552db6a1a1f04cf64933ff6b709474767
aherbert <a.herbert@sussex.ac.uk>
2023-03-28 12:28:49 +0100

    Deploy snapshot and create coverage report separately

--- .github/build.sh
+++ .github/build.sh
@@ -4,4 +4,4 @@ if mvn help:evaluate -Dexpression=project.version -q -DforceStdout | grep SNAPSH
     ARGS="-P snapshot-release --settings deploy/settings.xml deploy"
 fi
 
-mvn -V -B --no-transfer-progress -P jacoco,gdsc-test-doc,gdsc-test-examples spotbugs:check verify $ARGS
+mvn -V -B --no-transfer-progress -P jacoco spotbugs:check verify $ARGS
