fcb65d349970f237119c26371c23e00f077a0608
Alex Herbert <a.herbert@sussex.ac.uk>
2025-04-07 15:58:41 +0100

    Remove redundant build scripts

--- .github/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-ARGS="verify"
-if mvn help:evaluate -Dexpression=project.version -q -DforceStdout | grep SNAPSHOT; then
-    ARGS="-P snapshot-release --settings deploy/settings.xml deploy"
-fi
-
-mvn -V -B --no-transfer-progress -P jacoco spotbugs:check $ARGS
